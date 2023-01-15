200ade2fa84ecd3d27c3b83ca104dda33baa742d
Cyrille-Alexandre NDOUMBE <ndoumbecyrille@hotmail.com>
2023-01-15 17:48:06 +0100

    Added `PositiveInteger` type

---
+++ src/Candoumbe.Types/Numerics/PositiveInteger.cs
@@ -0,0 +1,174 @@
+﻿using System;
+
+#if NET7_0_OR_GREATER
+using System.Numerics;
+#endif
+
+namespace Candoumbe.Types.Numerics
+{
+    /// <summary>
+    /// A numeric type which value is garantied to never be less than <c>0</c>
+    /// </summary>
+    public record PositiveInteger : IEquatable<PositiveInteger>, IComparable<PositiveInteger>
+#if NET7_0
+        , IAdditionOperators<PositiveInteger, PositiveInteger, PositiveInteger>
+        , ISubtractionOperators<PositiveInteger, PositiveInteger, PositiveInteger>
+        , IMultiplyOperators<PositiveInteger, PositiveInteger, PositiveInteger>
+        , IComparisonOperators<PositiveInteger, PositiveInteger, bool>
+        , IAdditiveIdentity<PositiveInteger, PositiveInteger>
+        , IMultiplicativeIdentity<PositiveInteger, PositiveInteger>
+
+#endif
+    {
+#if NET7_0_OR_GREATER
+        ///<inheritdoc/>
+#else
+        /// <summary>
+        /// The additive identity of the current type
+        /// </summary> 
+#endif
+        public static PositiveInteger AdditiveIdentity => Zero;
+
+#if NET7_0_OR_GREATER
+        ///<inheritdoc/>
+#else
+        /// <summary>
+        /// The multiplicative identity of the current type
+        /// </summary> 
+#endif
+        public static PositiveInteger MultiplicativeIdentity => One;
+
+        /// <summary>
+        /// The zero value
+        /// </summary>
+        public static PositiveInteger Zero => new(0);
+
+        public static PositiveInteger One => new(1);
+
+
+        private PositiveInteger(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value), $"{nameof(value)} must be a positive or zero integer.");
+            }
+            Value = value;
+        }
+
+        /// <summary>
+        /// Gets the underlying value
+        /// </summary>
+        /// <remarks>The value is garantied to be &gt; or equal to <c>0</c></remarks>
+        public int Value { get; }
+
+        /// <summary>
+        /// Builds a new <see cref="PositiveInteger"/> with the given <paramref name="value"/>.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>a <see cref="PositiveInteger"/> which initial value is <paramref name="value"/>.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">if <paramref name="value"/> is <see langword="null"/>.</exception>
+        public static PositiveInteger From(int value) => new PositiveInteger(value);
+
+#if NET7_0_OR_GREATER
+        ///<inheritdoc/>
+#else
+        /// <summary>
+        /// Adds two values together and computes their sum
+        /// </summary>
+        /// <param name="left">The left value</param>
+        /// <param name="right">The right value</param>
+        /// <returns>the sum of <paramref name="left"/> and <paramref name="right"/>.</returns>
+#endif
+        public static PositiveInteger operator +(PositiveInteger left, PositiveInteger right) => new PositiveInteger(left.Value + right.Value);
+
+#if NET7_0_OR_GREATER
+        ///<inheritdoc/>
+#else
+        /// <summary>
+        /// Substracts <paramref name="right"/> from <paramref name="left"/>.
+        /// </summary>
+        /// <param name="left">The left value</param>
+        /// <param name="right">The right value</param>
+        /// <returns>the result of the difference of <paramref name="right"/> from <paramref name="left"/>.</returns>
+#endif
+        public static PositiveInteger operator -(PositiveInteger left, PositiveInteger right)
+            => new PositiveInteger((left.Value - right.Value) switch
+            {
+                < 0 => 0,
+                int result => result
+            });
+
+#if NET7_0_OR_GREATER
+        ///<inheritdoc/>
+#else
+        /// <summary>
+        /// Multiplies two values together to compute their product.
+        /// </summary>
+        /// <param name="left">The left value</param>
+        /// <param name="right">The right value</param>
+        /// <returns>The result of <paramref name="left"/> multiplied by <paramref name="right"/>.</returns>
+#endif
+        public static PositiveInteger operator *(PositiveInteger left, PositiveInteger right)
+            => new PositiveInteger(left.Value * right.Value);
+
+#if NET7_0_OR_GREATER
+        ///<inheritdoc/>
+#else
+        /// <summary>
+        /// Substracts <paramref name="right"/> from <paramref name="left"/>.
+        /// </summary>
+        /// <param name="left">The left value</param>
+        /// <param name="right">The right value</param>
+        /// <returns>the result of the difference of <paramref name="right"/> from <paramref name="left"/>.</returns>
+#endif
+        public static bool operator >(PositiveInteger left, PositiveInteger right) => left.Value > right.Value;
+
+#if NET7_0_OR_GREATER
+        ///<inheritdoc/>
+#else
+        /// <summary>
+        /// Compares <paramref name="right"/> and <paramref name="left"/> to decide which is the less.
+        /// </summary>
+        /// <param name="left">The left value</param>
+        /// <param name="right">The right value</param>
+        /// <returns><see langword="true"/> if <paramref name="left"/> is less than <paramref name="right"/> and <see langword="false"/> otherwise.</returns>
+#endif
+        public static bool operator <(PositiveInteger left, PositiveInteger right) => left.Value < right.Value;
+
+#if NET7_0_OR_GREATER
+        ///<inheritdoc/>
+#else
+        /// <summary>
+        /// Compares <paramref name="right"/> and <paramref name="left"/> to if <paramref name="left"/> is greater than or equal to <paramref name="right"/>.
+        /// </summary>
+        /// <param name="left">The left value</param>
+        /// <param name="right">The right value</param>
+        /// <returns><see langword="true"/> if <paramref name="left"/> is greather than or equal to <paramref name="right"/> and <see langword="false"/> otherwise.</returns>
+#endif
+        public static bool operator >=(PositiveInteger left, PositiveInteger right) => left.Value >= right.Value;
+
+#if NET7_0_OR_GREATER
+        ///<inheritdoc/>
+#else
+        /// <summary>
+        /// Compares <paramref name="right"/> and <paramref name="left"/> to decide if <paramref name="left"/> is less than or equal to <paramref name="right"/> .
+        /// </summary>
+        /// <param name="left">The left value</param>
+        /// <param name="right">The right value</param>
+        /// <returns><see langword="true"/> if <paramref name="left"/> is less than or equal to <paramref name="right"/> and <see langword="false"/> otherwise.</returns>
+#endif
+        public static bool operator <=(PositiveInteger left, PositiveInteger right) => left.Value <= right.Value;
+
+        ///<inheritdoc/>
+        public override int GetHashCode() => Value.GetHashCode();
+
+        ///<inheritdoc/>
+        public int CompareTo(PositiveInteger other)
+        {
+            return other is null
+                ? throw new ArgumentNullException(nameof(other), $"{nameof(other)} cannot be null")
+                : Value.CompareTo(other.Value);
+        }
+    }
+
+}
---
+++ test/Candoumbe.Types.UnitTests/Numerics/PositiveIntegerTests.cs
@@ -0,0 +1,130 @@
+﻿namespace Candoumbe.Types.UnitTests.Numerics;
+
+using Candoumbe.Types.Numerics;
+
+using FluentAssertions;
+
+using FsCheck;
+using FsCheck.Xunit;
+
+using System;
+
+public class PositiveIntegerTests
+{
+    [Property]
+    public void Given_PositiveIntegers_When_Addition_Then_ShouldReturnCorrectResult(PositiveInt leftValue, PositiveInt rightValue)
+    {
+        // Arrange
+        int left = leftValue.Get;
+        int right = rightValue.Get;
+        PositiveInteger positiveA = PositiveInteger.From(left);
+        PositiveInteger positiveB = PositiveInteger.From(right);
+
+        // Act
+        PositiveInteger result = positiveA + positiveB;
+
+        // Assert
+        result.Value.Should().Be(left + right);
+    }
+
+    [Property]
+    public void Given_PositiveIntegers_When_Subtraction_Then_ShouldReturnCorrectResult(int left, int right)
+    {
+        // Arrange
+        left = Math.Max(0, left);
+        right = Math.Max(0, right);
+        PositiveInteger positiveA = PositiveInteger.From(left);
+        PositiveInteger positiveB = PositiveInteger.From(right);
+
+        // Act
+        PositiveInteger result = positiveA - positiveB;
+
+        // Assert
+        object _ = (left - right) switch
+        {
+            < 0 => result.Value.Should().Be(0, "a positive integer can never hold a negative value"),
+            int value => result.Value.Should().Be(value)
+        };
+    }
+
+    [Property]
+    public void Given_PositiveIntegers_When_Multiplication_Then_ShouldReturnCorrectResult(int left, int right)
+    {
+        // Arrange
+        left = Math.Max(0, left);
+        right = Math.Max(0, right);
+        PositiveInteger positiveA = PositiveInteger.From(left);
+        PositiveInteger positiveB = PositiveInteger.From(right);
+
+        // Act
+        PositiveInteger result = positiveA * positiveB;
+
+        // Assert
+        result.Value.Should().Be(left * right);
+    }
+
+    [Property]
+    public void Given_PositiveIntegers_When_LessThan_Then_ShouldReturnCorrectResult(int left, int right)
+    {
+        // Arrange
+        left = Math.Max(0, left);
+        right = Math.Max(0, right);
+        PositiveInteger positiveA = PositiveInteger.From(left);
+        PositiveInteger positiveB = PositiveInteger.From(right);
+
+        // Act
+        bool result = positiveA < positiveB;
+
+        // Assert
+        result.Should().Be(left < right);
+    }
+
+    [Property]
+    public void Given_PositiveIntegers_When_LessThanOrEqual_Then_ShouldReturnCorrectResult(int left, int right)
+    {
+        // Arrange
+        left = Math.Max(0, left);
+        right = Math.Max(0, right);
+        PositiveInteger positiveA = PositiveInteger.From(left);
+        PositiveInteger positiveB = PositiveInteger.From(right);
+
+        // Act
+        bool result = positiveA <= positiveB;
+
+        // Assert
+        result.Should().Be(left <= right);
+    }
+
+    [Property]
+    public void Given_PositiveIntegers_When_GreaterThan_Then_ShouldReturnCorrectResult(int left, int right)
+    {
+        // Arrange
+        left = Math.Max(0, left);
+        right = Math.Max(0, right);
+        PositiveInteger positiveA = PositiveInteger.From(left);
+        PositiveInteger positiveB = PositiveInteger.From(right);
+
+        // Act
+        bool result = positiveA > positiveB;
+
+        // Assert
+        result.Should().Be(left > right);
+    }
+
+    [Property]
+    public void Given_PositiveIntegers_When_GreaterThanOrEqual_Then_ShouldReturnCorrectResult(int left, int right)
+    {
+        // Arrange
+        left = Math.Max(0, left);
+        right = Math.Max(0, right);
+        PositiveInteger positiveA = PositiveInteger.From(left);
+        PositiveInteger positiveB = PositiveInteger.From(right);
+
+        // Act
+        bool result = positiveA >= positiveB;
+
+        // Assert
+        result.Should().Be(left >= right);
+    }
+
+}
