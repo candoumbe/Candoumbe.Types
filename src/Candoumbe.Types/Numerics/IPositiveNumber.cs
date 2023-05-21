9b20095ca354fa21ae114acb7b016ba0d359a11c
Cyrille-Alexandre NDOUMBE <ndoumbecyrille@hotmail.com>
2023-05-21 22:20:37 +0200

    feat(numerics) : add new `IPositiveNumber` type

---
+++ src/Candoumbe.Types/Numerics/IPositiveNumber.cs
@@ -0,0 +1,24 @@
+﻿#if NET7_0_OR_GREATER
+using System.Numerics;
+#endif
+
+namespace Candoumbe.Types.Numerics
+{
+#if NET7_0_OR_GREATER
+    /// <summary>
+    /// Base class for creating numeric types that can only be positive
+    /// </summary>
+    /// <typeparam name="TNumber">The numeric type that the current type will restrict to only positive values.</typeparam>
+    /// <typeparam name="TSelf">The positive number type</typeparam>
+    public interface IPositiveNumber<TNumber, TSelf>
+        where TNumber : ISignedNumber<TNumber>
+        where TSelf : IPositiveNumber<TNumber, TSelf>, IMinMaxValue<TSelf>, IComparisonOperators<TSelf, TSelf, bool>
+    {
+        /// <summary>
+        /// Gets the underlying value
+        /// </summary>
+        /// <remarks>The value is garantied to be &gt; or equal to <c>0</c></remarks>
+        TNumber Value { get; }
+    }
+#endif
+}
