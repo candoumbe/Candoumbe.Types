5b648c0b510811eac11c65ae5c27af5bad48b154
Cyrille NDOUMBE <ndoumbecyrille@gmail.com>
2024-11-04 12:32:36 +0100

    refactor: small code refactoring and documentation update

--- src/Candoumbe.Types/IRange.cs
+++ src/Candoumbe.Types/IRange.cs
@@ -4,8 +4,8 @@ namespace Candoumbe.Types;
 
 /// <summary>
 /// It's quite common to see comparisons where a value is checked against an interval of values. Ranges are most of the time handled by a pair of values,
-/// and you check against them both. <see cref="IRangeRange{TInterval,TBound}"/> instead uses a single object to represent the range as a whole, and then provides the relevant operations
-/// to check if values fall in the <see cref="IRangeRange{TInterval,TBound}"/> and to compare <see cref="IRangeRange{TInterval,TBound}"/>s.
+/// and you check against them both. <see cref="IRange{TInterval,TBound}"/> instead uses a single object to represent the range as a whole, and then provides the relevant operations
+/// to check if values fall in the <see cref="IRange{TInterval,TBound}"/> and to compare <see cref="IRange{TInterval,TBound}"/>s.
 /// </summary>
 public interface IRange<TInterval, TBound> : IComparable<TInterval>, IEquatable<TInterval>
     where TInterval : IRange<TInterval, TBound>
