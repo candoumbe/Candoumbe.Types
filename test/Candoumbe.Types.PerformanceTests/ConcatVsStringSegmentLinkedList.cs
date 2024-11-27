a5608684ce3536a6a19d1819e80926aeae859634
Cyrille NDOUMBE <ndoumbecyrille@gmail.com>
2024-11-28 00:42:06 +0100

    ci: add workflow to run performance tests
    add workflow to run performance tests
    add performance tests for `StringSegmentList.Replace(char, char)`
    remove `net6.0` and `net9.0` performance tests

--- test/Candoumbe.Types.PerformanceTests/ConcatVsStringSegmentLinkedList.cs
+++ test/Candoumbe.Types.PerformanceTests/ConcatVsStringSegmentLinkedList.cs
@@ -8,9 +8,7 @@ using Candoumbe.Types.Strings;
 namespace Candoumbe.Types.PerformanceTests;
 
 [MemoryDiagnoser]
-[SimpleJob(RuntimeMoniker.Net60)]
 [SimpleJob(RuntimeMoniker.Net80)]
-[SimpleJob(RuntimeMoniker.Net90)]
 public class ConcatVsStringSegmentLinkedList
 {
     [Params(10, 100, 1000)]
