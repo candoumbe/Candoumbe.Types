194e7279aae8b375eb84501c8c77e6c78e22c6b7
Cyrille NDOUMBE <ndoumbecyrille@gmail.com>
2025-07-15 16:59:56 +0200

    chore(tests): remove old unit test project/files

--- test/Candoumbe.Types.UnitTests/Strings/StringSegmentLinkedListState.cs
+++
@@ -1,7 +0,0 @@
-namespace Candoumbe.Types.UnitTests.Strings;
-
-public record StringSegmentLinkedListState(string[] Inputs, string Value)
-{
-    /// <inheritdoc />
-    public override string ToString() => $@"Inputs: [{string.Join(", ", Inputs)}], Value: ""{Value}""";
-}
